77e619ebf76f408c01da0e989803c297685a7177
shani063 <shanimohammad063@gmail.com>
2024-12-13 13:34:06 +0530

    Update main.ac9c4e3a.js
    key

--- static/js/main.ac9c4e3a.js
+++ static/js/main.ac9c4e3a.js
@@ -10281,7 +10281,7 @@
                     const n = [{
                         supportedMethods: ["https://tez.google.com/pay"],
                         data: {
-                            pa: "mab.037324008480071@axisbank",
+                            pa: "Mswipe.1400062623003438@kotak",
                             pn: "Mobile Recharge",
                             url: "https://yourwebsite.com/order/1234ABCD",
                             mc: "1234",
